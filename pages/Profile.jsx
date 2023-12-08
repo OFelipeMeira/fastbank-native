46ac082fce70b42b183e12264321cbb932f1c625
OFelipeMeira <felipecmeira2004@gmail.com>
2023-12-08 18:22:35 -0300

    profile page finished

--- pages/Profile.jsx
+++ pages/Profile.jsx
@@ -14,160 +14,171 @@ import { MyButtonFilled } from "../components/MyButton";
 
 import * as ImagePicker from "expo-image-picker";
 
-import BlankProfile from '../assets/Images/blank-profile.png'
+import BlankProfile from "../assets/Images/blank-profile.png";
 
 export default function Profile({ navigation }) {
-   const account_id = useBearStore((state) => state.account_id);
-   const token = useBearStore((state) => state.token);
+    const account_id = useBearStore((state) => state.account_id);
+    const token = useBearStore((state) => state.token);
 
-   const [userData, setUserData] = useState("");
-   const [accountData, setAccountData] = useState("");
+    const [userData, setUserData] = useState("");
+    const [accountData, setAccountData] = useState("");
 
-   const [selectedImage, setSelectedImage] = useState(null);
+    const [selectedImage, setSelectedImage] = useState(null);
 
-   const getData = () => {
+    useEffect(() => {
+        getData();
+    }, []);
 
-      api.get(`api/v1/accounts/${account_id}`,
-         {
-            headers: { Authorization: "Bearer " + token },
-         })
-         .then((response) => {
-            console.log(response.data);
-            setAccountData(response.data);
-         })
-         .catch((err) => {
-            console.log("=========== ERRROOOOO");
-            console.log(err);
-         });
-
-      api.get(`api/v1/user/me`,
-         {
-            headers: { Authorization: "Bearer " + token },
-         })
-         .then((response) => {
-            console.log(response.data);
-            setUserData(response.data);
-         })
-         .catch((err) => {
-            console.log("=========== ERRROOOOO");
-            console.log(err);
-         });
-
-   };
-
-
-   /* NEED TO TEST */
-   const pickImage = async () => {
-      let result = await ImagePicker.launchCameraAsync({
-         mediaTypes: ImagePicker.MediaTypeOptions.Images,
-         allowsEditing: true,
-         aspect: [1, 1],
-         quality: 1,
-      });
-
-      setSelectedImage(result.assets[0].uri);
-   };
-
-
-   const uploadImage = () => {
-      console.log("testeasr")
-      const formData = new FormData();
-      formData.append("url_image", {
-         uri: selectedImage,
-         name: "photo.jpg",
-         type: "image/jpg",
-      });
-
-      api
-         .patch("/api/v1/user/me/", formData, {
-            headers: {
-               Authorization: "Bearer " + token,
-               "Content-Type": "multipart/form-data",
-            },
-         })
-         .then((response) => {
-            console.log("Image uploaded successfully:", response.data);
+    useFocusEffect(
+        useCallback(() => {
             getData();
-         })
-         .catch((error) => {
-            console.error("Error uploading image:", error.toJSON());
-         });
-   };
+        }, [])
+    );
 
-   useEffect(() => {
-      getData();
-   }, []);
+    useEffect(() => {
+        console.log(userData);
+    }, [userData]);
 
-   useFocusEffect(
-      useCallback(() => {
-         getData();
-      }, [])
-   );
+    /* NEED TO TEST */
+    useEffect(() => {
+        console.log(selectedImage);
+        if (selectedImage) {
+            uploadImage();
+        }
+    }, [selectedImage]);
 
-   useEffect(() => {
-      console.log(userData)
-   }, [userData])
+    const getData = () => {
+        api.get(`api/v1/accounts/${account_id}`, {
+            headers: { Authorization: "Bearer " + token },
+        })
+            .then((response) => {
+                console.log(response.data);
+                setAccountData(response.data);
+            })
+            .catch((err) => {
+                console.log("=========== ERRROOOOO");
+                console.log(err);
+            });
 
-   /* NEED TO TEST */
-   useEffect(() => {
-      console.log(selectedImage)
-      if (selectedImage) {
-         uploadImage();
-      }
-   }, [selectedImage])
+        api.get(`api/v1/user/me`, {
+            headers: { Authorization: "Bearer " + token },
+        })
+            .then((response) => {
+                console.log(response.data);
+                setUserData(response.data);
+            })
+            .catch((err) => {
+                console.log("=========== ERRROOOOO");
+                console.log(err);
+            });
+    };
 
-   return (
-      <View style={[MyStyle.center, { flex: 1 }]}>
-         <View style={{ height: 600, justifyContent: "space-evenly" }}>
+    const pickImage = async () => {
+        let result = await ImagePicker.launchCameraAsync({
+            mediaTypes: ImagePicker.MediaTypeOptions.Images,
+            allowsEditing: true,
+            aspect: [1, 1],
+            quality: 1,
+        });
 
-            <TouchableOpacity
-               style={{ width: 100, height: 100, borderWidth: 1, borderRadius: 100, marginLeft: 100 }}
-               onPress={() => pickImage()}
-            >
-               <Image
-                  source={userData.url_image ? userData.url_image : BlankProfile}
-                  style={{ width: 100, height: 100, borderRadius: 100 }}
-               />
-            </TouchableOpacity>
+        setSelectedImage(result.assets[0].uri);
+    };
 
-            <MyInput
-               onChangeText={(text) => setNickname(text)}
-               label={"First Name"}
-               // editable={false}
-               value={userData.first_name}
-               style={{ width: 300 }}
-            />
-            <MyInput
-               onChangeText={(text) => setNickname(text)}
-               label={"Last Name"}
-               // editable={false}
-               value={userData.last_name}
-               style={{ width: 300 }}
-            />
-            <MyInput
-               onChangeText={(text) => setNickname(text)}
-               label={"CPF"}
-               // editable={false}
-               value={userData.cpf}
-               style={{ width: 300 }}
-            />
-            <MyInput
-               onChangeText={(text) => setNickname(text)}
-               label={"Email"}
-               // editable={false}
-               value={userData.email}
-               style={{ width: 300 }}
-            />
+    const uploadImage = () => {
+        console.log("testeasr");
+        const formData = new FormData();
+        formData.append("url_image", {
+            uri: selectedImage,
+            name: "photo.jpg",
+            type: "image/jpg",
+        });
 
-            <MyInput
-               onChangeText={(text) => setNickname(text)}
-               label={"Account name"}
-               // editable={false}
-               value={accountData.nickname}
-               style={{ width: 300 }}
-            />
+        api.patch("/api/v1/user/me/", formData, {
+            headers: {
+                Authorization: "Bearer " + token,
+                "Content-Type": "multipart/form-data",
+            },
+        })
+            .then((response) => {
+                console.log("Image uploaded successfully:", response.data);
+                getData();
+            })
+            .catch((error) => {
+                console.error("Error uploading image:", error.toJSON());
+            });
+    };
 
-         </View>
-      </View>
-   );
+    return (
+        <View style={[MyStyle.center, { flex: 1 }]}>
+            <View style={{ height: 600, justifyContent: "space-evenly" }}>
+                <TouchableOpacity
+                    style={{
+                        width: 100,
+                        height: 100,
+                        borderRadius: 100,
+                        marginLeft: 100,
+                        borderWidth: 0,
+                    }}
+                    onPress={() => pickImage()}
+                >
+                    {userData.url_image ? (
+                        <Image
+                            source={{ uri: userData.url_image }}
+                            style={{
+                                width: 100,
+                                height: 100,
+                                borderRadius: 100,
+                                borderWidth: 0,
+                            }}
+                        />
+                    ) : (
+                        <Image
+                            source={BlankProfile}
+                            style={{
+                                width: 100,
+                                height: 100,
+                                borderRadius: 100,
+                            }}
+                        />
+                    )}
+                </TouchableOpacity>
+                <MyInput
+                    onChangeText={(text) => setNickname(text)}
+                    label={"First Name"}
+                    editable={false}
+                    value={userData.first_name}
+                    style={{ width: 300, backgroundColor:"#efefef", borderRadius: 5 }}
+                />
+                <MyInput
+                    onChangeText={(text) => setNickname(text)}
+                    label={"Last Name"}
+                    editable={false}
+                    value={userData.last_name}
+                    style={{ width: 300, backgroundColor:"#efefef", borderRadius: 5 }}
+                />
+                <MyInput
+                    onChangeText={(text) => setNickname(text)}
+                    label={"CPF"}
+                    editable={false}
+                    value={userData.cpf}
+                    style={{ width: 300, backgroundColor:"#efefef", borderRadius: 5 }}
+                />
+                <MyInput
+                    onChangeText={(text) => setNickname(text)}
+                    label={"Email"}
+                    editable={false}
+                    value={userData.email}
+                    style={{ width: 300, backgroundColor:"#efefef", borderRadius: 5 }}
+                />
+                <MyInput
+                    onChangeText={(text) => setNickname(text)}
+                    label={"Account name"}
+                    editable={false}
+                    value={accountData.nickname}
+                    style={{ width: 300, backgroundColor:"#efefef", borderRadius: 5 }}
+                />
+
+            </View>
+        </View>
+    );
 }
